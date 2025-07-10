874c15ba0f0f873e76239ad8057f24c25618d705
yeoro <dufgh1009@naver.com>
2025-07-10 22:07:00 +0900

    Initial commit

---
+++ src/test/java/yeoro/splearn/SplearnApplicationTests.java
@@ -0,0 +1,13 @@
+package yeoro.splearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplearnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
