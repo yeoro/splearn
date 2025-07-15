b4baba3472731054c35c7bf7293a063d1ebf44a9
yeoro <dufgh1009@gmail.com>
2025-07-15 12:56:31 +0900

    Member 도메인 로직 개발

--- src/main/java/yeoro/splearn/domain/Member.java
+++ src/main/java/yeoro/splearn/domain/Member.java
@@ -1,11 +1,14 @@
 package yeoro.splearn.domain;
 
 import lombok.Getter;
-import org.springframework.util.ObjectUtils;
+import lombok.ToString;
 
 import java.util.Objects;
 
+import static org.springframework.util.Assert.state;
+
 @Getter
+@ToString
 public class Member {
     private String email;
 
@@ -22,4 +25,15 @@ public class Member {
         this.status = MemberStatus.PENDING;
     }
 
+    public void activate() {
+        state(status == MemberStatus.PENDING, "PENDING 상태가 아닙니다");
+
+        this.status = MemberStatus.ACTIVE;
+    }
+
+    public void deactivate() {
+        state(status == MemberStatus.ACTIVE, "ACTIVE 상태가 아닙니다");
+
+        this.status = MemberStatus.DEACTIVATED;
+    }
 }
--- src/test/java/yeoro/splearn/domain/MemberTest.java
+++ src/test/java/yeoro/splearn/domain/MemberTest.java
@@ -19,4 +19,46 @@ class MemberTest {
         assertThatThrownBy(() -> new Member(null, "yeoro", "secret"))
                 .isInstanceOf(NullPointerException.class);
     }
+    
+    @Test
+    void activate() {
+        var member = new Member("yeoro@splearn.app", "yeoro", "secret");
+
+        member.activate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.ACTIVE);
+    }
+
+    @Test
+    void activateFail() {
+        var member = new Member("yeoro@splearn.app", "yeoro", "secret");
+
+        member.activate();
+
+        assertThatThrownBy(() -> {
+            member.activate();
+        }).isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void deactivate() {
+        var member = new Member("yeoro@splearn.app", "yeoro", "secret");
+        member.activate();
+
+        member.deactivate();
+
+        assertThat(member.getStatus()).isEqualTo(MemberStatus.DEACTIVATED);
+    }
+
+    @Test
+    void deactivateFail() {
+        var member = new Member("yeoro@splearn.app", "yeoro", "secret");
+
+        assertThatThrownBy(() -> member.deactivate()).isInstanceOf(IllegalStateException.class);
+
+        member.activate();
+        member.deactivate();
+
+        assertThatThrownBy(() -> member.deactivate()).isInstanceOf(IllegalStateException.class);
+    }
 }
